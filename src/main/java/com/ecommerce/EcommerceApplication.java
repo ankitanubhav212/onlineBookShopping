93b1849b1ffef5845c284b08d3ffb7ff449d0aa5
ankit-zumen <ankit@zumen.in>
2021-01-25 07:26:14 +0530

    db props

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -31,33 +31,36 @@ public class EcommerceApplication implements ApplicationRunner {
 			RestTemplate restTemplate = new RestTemplate();
 			HttpHeaders headers = new HttpHeaders();
 			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-			ResponseEntity<List<Map<String, Object>>> response=  restTemplate.exchange("https://s3-ap-southeast-1.amazonaws.com/he-public-data/books8f8fe52.json", HttpMethod.GET,null,  new ParameterizedTypeReference<List<Map<String,Object>>>(){});
-			List<Map<String,Object>> responseBody = response.getBody();
-			List<Product> products = new ArrayList<>();
-			System.out.println("Fetching list of items");
-			responseBody.forEach(value->{
-				Product product = new Product();
-				product.setBookID((Integer) value.get("bookID"));
-				product.setTitle(String.valueOf(value.get("title")));
-				product.setAuthors((String) value.get("authors"));
-				product.setPrice(Double.valueOf(Integer.valueOf(String.valueOf(value.get("price")))));
-				try {
-					product.setAverageRating((Double) value.get("average_rating"));
-				} catch (Exception e) {
-					product.setAverageRating(Double.valueOf(0));
-				}
+			if(productRepository.findAll().size() == 0) {
+				ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange("https://s3-ap-southeast-1.amazonaws.com/he-public-data/books8f8fe52.json", HttpMethod.GET, null, new ParameterizedTypeReference<List<Map<String, Object>>>() {
+				});
+				List<Map<String, Object>> responseBody = response.getBody();
+				List<Product> products = new ArrayList<>();
+				System.out.println("Fetching list of items");
+				responseBody.forEach(value -> {
+					Product product = new Product();
+					product.setBookID((Integer) value.get("bookID"));
+					product.setTitle(String.valueOf(value.get("title")));
+					product.setAuthors((String) value.get("authors"));
+					product.setPrice(Double.valueOf(Integer.valueOf(String.valueOf(value.get("price")))));
+					try {
+						product.setAverageRating((Double) value.get("average_rating"));
+					} catch (Exception e) {
+						product.setAverageRating(Double.valueOf(0));
+					}
 
-				try {
-					product.setRatingsCount(Integer.valueOf(value.get("ratings_count").toString()));
-				} catch (Exception e) {
-					product.setRatingsCount(0);
-				}
-				product.setIsbn(String.valueOf(value.get("isbn")));
-				product.setLanguage(String.valueOf(value.get("language_code")));
-				products.add(product);
-			});
-		productRepository.saveAll(products.subList(0,1000));
-		System.out.println("All Item fetched succesfully");
+					try {
+						product.setRatingsCount(Integer.valueOf(value.get("ratings_count").toString()));
+					} catch (Exception e) {
+						product.setRatingsCount(0);
+					}
+					product.setIsbn(String.valueOf(value.get("isbn")));
+					product.setLanguage(String.valueOf(value.get("language_code")));
+					products.add(product);
+				});
+				productRepository.saveAll(products.subList(0, 1000));
+				System.out.println("All Item fetched succesfully");
+			}
 	}
 
 }
